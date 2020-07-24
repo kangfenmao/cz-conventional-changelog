a5ee2072764058eba2f03033740ac9c46757720e
亢奋猫 <elekids@qq.com>
2020-07-24 16:29:03 +0800

    Remove some confirm quetion

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'format cjs';
 
 var engine = require('./engine');
-var conventionalCommitTypes = require('conventional-commit-types');
+var conventionalCommitTypes = require('./index.json');
 var configLoader = require('commitizen').configLoader;
 
 var config = configLoader.load() || {};
